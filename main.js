9d05ce7d8151a1a46bca4124e37951921ee07ebb
alisha0217 <69092959+alisha0217@users.noreply.github.com>
2021-09-14 20:44:01 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+ScoreLeftWrist = 0;
+ScoreRightWrist = 0;
+
 song="";
 leftWristX = 0;
 leftWristY = 0;
@@ -7,12 +10,6 @@ rightWristY = 0;
 
 function preload(){
     song = loadSound("music.mp3");
-}
-
-function play(){
-    song.play();
-    song.setVolume(1);
-    song.rate(1);
 }
 
 function setup(){
@@ -28,6 +25,41 @@ function setup(){
 
 function draw(){
     image(video, 0, 0, 600, 500);
+    fill("#ff0000");
+    stroke("#ff0000");
+    circle(rightWristX,rightWristY,20);
+
+    if(rightWristY > 0 && rightWristY <= 100){
+        document.getElementById("speed").innerHTML = "Speed = 0.5x";
+        song.rate(0.5); 
+    }
+    else if(rightWristY > 100 && rightWristY <= 200){
+        document.getElementById("speed").innerHTML = "Speed = 1x";
+        song.rate(1); 
+    }
+    else if(rightWristY > 200 && rightWristY <= 300){
+        document.getElementById("speed").innerHTML = "Speed = 1.5x";
+        song.rate(1.5); 
+    }
+    else if(rightWristY > 300 && rightWristY <= 400){
+        document.getElementById("speed").innerHTML = "Speed = 2x";
+        song.rate(2); 
+    }
+    else if(rightWristY > 400 && rightWristY <= 500){
+        document.getElementById("speed").innerHTML = "Speed = 2.5x";
+        song.rate(2.5); 
+    }
+
+
+    if(scoreLeftWrist > 0.2){
+        circle(leftWristX,leftWristY,20);
+        InNumberleftWristY = Number(leftWristY);
+        remove_decimals = floor(InNumberleftWristY);
+        volume = remove_decimal / 500;
+        document.getElementById("volume").innerHTML = "volume = "  + volume;
+        song.setVolume(volume);
+    }
+   
 }
 
 function modelLoaded(){
@@ -48,5 +80,13 @@ function gotPoses(results){
         rightWristY = results[0].pose.rightWrist.y;
 
         console.log("rightWristX = " + rightWristX + " rightWristY = " + rightWristY );
+
     }
 }
+
+function play(){
+    song.play();
+    song.setVolume(1);
+    song.rate(1);
+}
+
